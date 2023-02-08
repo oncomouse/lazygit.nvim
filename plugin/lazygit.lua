93efb2c374ed814321ca0103e174ca9717f1175f
oncomouse <oncomouse@gmail.com>
2023-02-08 14:00:56 -0600

    fix: spelled <plug> binding correctly

--- plugin/lazygit.lua
+++ plugin/lazygit.lua
@@ -1,4 +1,4 @@
 -- Create it as a command, :LazyGit
 vim.api.nvim_create_user_command("LazyGit", require("lazygit"), {})
 -- Create it as a <Plug> binding:
-vim.keymap.set("n", "<Plug>(layzgit.nvim)", "<cmd>lua require('lazygit')()<cr>")
+vim.keymap.set("n", "<Plug>(lazygit.nvim)", "<cmd>lua require('lazygit')()<cr>")
