df8525950668870c3a4124c8879870a49ccc182a
oncomouse <oncomouse@gmail.com>
2023-02-09 11:58:14 -0600

    feat: :LazyGit triggers the <Plug> binding, which simplifies lazy loading.

--- plugin/lazygit.lua
+++ plugin/lazygit.lua
@@ -1,4 +1,4 @@
--- Create it as a command, :LazyGit
-vim.api.nvim_create_user_command("LazyGit", require("lazygit"), {})
 -- Create it as a <Plug> binding:
 vim.keymap.set("n", "<Plug>(lazygit.nvim)", "<cmd>lua require('lazygit')()<cr>")
+-- Create it as a command, :LazyGit
+vim.api.nvim_create_user_command("LazyGit", [[execute "normal! \<Plug>(lazygit.nvim)"]], {})
